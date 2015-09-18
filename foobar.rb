4c7e6e40fc52026887d1cd55c01f37c37da2fb94
Juno Student <j.morey@berkeley.edu>
2015-09-18 14:54:28 -0700

    hw2 q3

--- foobar.rb
+++ foobar.rb
@@ -1,10 +1,6 @@
 class Foobar
-  def baz(ary)
-  	result = Array.new
-  	for str_num in ary
-  		num = str_num.to_i + 2
-  		if (num % 2 == 0) and (not result.include? num) and (num < 10) 
-  			result += num
-  	sum = 0
-  	return result.inject{|sum, x| sum + x}
+	def baz(arr)
+		arr.map! {|elem| elem.to_i + 2} 
+		return arr
+	end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,22 +1,32 @@
 def squared_sum(a, b)
-  (a + b) ** 2
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
-  prev = a[0]
-  i = 1
-  while i < a.length 
-  	if a[i] < prev
-  		a[i-1], a[i] = a[i], a[i-1]
-  	i += 1
+	sorted = a.sort 
+	sorted.map! { |x| x + 1}
+	return sorted
 end
 
+
 def combine_name(first_name, last_name)
-  first_name + " " + last_name
+  return first_name + " " + last_name
 end
+
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
 end
+
+
+
+
+
+
+
+
+
+
+
