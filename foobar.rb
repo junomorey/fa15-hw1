73b3c644ea983df8bb5db7abbede1b5692b1afbe
Juno Student <j.morey@berkeley.edu>
2015-09-18 18:17:54 -0700

    hw1 finished

--- foobar.rb
+++ foobar.rb
@@ -1,6 +1,9 @@
 class Foobar
-	def baz(arr)
-		arr.map! {|elem| elem.to_i + 2} 
-		return arr
+	def self.baz(arr)
+		arr.map! { |elem| elem.to_i + 2} 
+		arr.keep_if { |elem| elem % 2 == 0}
+		arr.uniq
+		arr.keep_if { |elem| elem < 10}
+		return arr.reduce :+
 	end
 end
